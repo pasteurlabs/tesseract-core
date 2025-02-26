4e3186c1503633d698eefc1949e9d4c1d50789b9
Matteo Salvador <msalvad@stanford.edu>
2025-02-26 12:31:44 +0100

    Context manager for Julia example

--- examples/sciml/julia_mlp/run_served_tesseract_api.py
+++ examples/sciml/julia_mlp/run_served_tesseract_api.py
@@ -4,44 +4,9 @@
 # Simple development file used to show how to make requests to the julia_flux_mlp tesseract
 # assumes the tesseract is located at: http://127.0.0.1:8000
 
-import base64
+from tesseract_core import Tesseract
 
-import numpy as np
-import requests
-
-
-class Client:
-    def __init__(self, host: str = "127.0.0.1", port: int = 8000) -> None:
-        self.host = host
-        self.port = port
-
-    @property
-    def get_base_api_url(self) -> str:
-        return f"http://{self.host}:{self.port}/"
-
-    def request(self, endpoint: str, method: str = "GET", payload=None) -> dict:
-        url = self.get_base_api_url + endpoint.lstrip("/")
-
-        if payload is not None:
-            body_json = {k: v for k, v in payload.items() if v is not None}
-        else:
-            body_json = None
-
-        response = requests.request(method=method, url=url, json=body_json)
-        response.raise_for_status()
-        data = response.json()
-
-        return data
-
-
-# NOTE: pass port argument to wherever the tesseract is served
-client = Client()
-
-data = client.request(
-    "apply",
-    method="POST",
-    payload={"inputs": {"n_epochs": 1}},
-)
-buffer = data["state"]["data"]["buffer"]
-decoded_buffer = base64.b64decode(buffer)
-result = np.frombuffer(decoded_buffer)
+with Tesseract.from_image(image="julia_flux_mlp") as julia_flux_mlp:
+    data = julia_flux_mlp.apply(inputs={"n_epochs": 10})
+state = data["state"]
+print(data["metrics"])
