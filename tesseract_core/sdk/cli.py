ecf33630ca3e59a014a01030106dc4602d3bd0ce
Dion Häfner <dion.haefner@simulation.science>
2025-03-20 16:26:51 +0100

    add information on printed values

--- tesseract_core/sdk/cli.py
+++ tesseract_core/sdk/cli.py
@@ -272,6 +272,9 @@ def build_image(
 
     The passed directory must contain the files `tesseract_api.py` and `tesseract_config.yaml`
     (can be created via `tesseract init`).
+
+    Prints the built images as JSON array to stdout, for example: `["mytesseract:latest"]`.
+    If `--generate-only` is set, the path to the build context is printed instead.
     """
     if config_override is None:
         config_override = []
