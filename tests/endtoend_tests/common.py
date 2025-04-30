265ed01cdbcf7c556d6c856a4c77e1e24c1ef878
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2025-04-30 12:47:53 -0300

    Handle image_name with URL

--- tests/endtoend_tests/common.py
+++ tests/endtoend_tests/common.py
@@ -74,5 +74,5 @@ def build_tesseract(
     assert result.exit_code == 0, result.exception
 
     image_tags = json.loads(result.stdout.strip())
-    assert image_name in image_tags
+    assert [image_name in tag for tag in image_tags]
     return image_tags[0]
