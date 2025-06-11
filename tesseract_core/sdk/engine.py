0b0aedcdeb1026dd1eae23501121610cfac44c1a
Angela Ko <angela.ko@simulation.science>
2025-06-10 21:15:59 -0400

    fix: Add FileNotFoundError to docker info (#215)
    #### Relevant issue or PR
    #157 
    
    #### Description of changes
    Added FileNotFound exception to `needs_docker` wrapper
    
    #### Testing done
    Completely uninstalled docker and triggered the FileNotFoundError / no
    docker command error.
    
    Reinstalled package with change and now we get this error instead.
    
    ```
    (tesseract) 柯 ~/work/tesseract-core » tesseract build examples/empty
     [-] Docker daemon is not running, check if it is installed.
     [x] Aborting
     ```
    
    ---------
    
    Co-authored-by: Alessandro Angioi <alessandro.angioi@simulation.science>

--- tesseract_core/sdk/engine.py
+++ tesseract_core/sdk/engine.py
@@ -113,6 +113,8 @@ def needs_docker(func: Callable) -> Callable:
             raise UserError(
                 "Could not reach Docker daemon, check if it is running."
             ) from ex
+        except FileNotFoundError as ex:
+            raise UserError("Docker not found, check if it is installed.") from ex
         return func(*args, **kwargs)
 
     return wrapper_needs_docker
