b070cdbcf44ce60e6c234b30c036161561918bc4
Angela Ko <angela.ko@simulation.science>
2025-06-13 11:16:32 -0400

    Test removing none statement

--- tesseract_core/sdk/config.py
+++ tesseract_core/sdk/config.py
@@ -80,4 +80,8 @@ _current_config = None
 
 def get_config() -> RuntimeConfig:
     """Return the current runtime configuration."""
+    global _current_config
+
+    if _current_config is None:
+        update_config()
     return _current_config
