c030ab058a44e3d25d5a500329830462cf68a9e3
andrinr <andrinrehmann@gmail.com>
2025-11-19 10:46:25 +0100

    sorry, wrong branch

--- examples/qoi_wf/Readme.md
+++
@@ -1,3 +0,0 @@
-1. New conda env
-2. Install tesseract-core[runtime], torch
-3. Run test.py to test
--- examples/reproducer/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-tesseract --loglevel debug build .
-
-docker system prune --force
-
-tesseract run reproducer apply '{"inputs":{}}' --output-path outputs
