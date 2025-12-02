551980d110111407cae0c7eae36bbe049fff0236
Dion Häfner <dion.haefner@simulation.science>
2025-12-02 13:26:27 +0100

    again

--- .github/workflows/get_bump_type.sh
+++ .github/workflows/get_bump_type.sh
@@ -10,6 +10,6 @@ current_major=$(cut -d '.' -f 1 <<< "$current_version")
 new_major=$(cut -d '.' -f 1 <<< "$new_version")
 if [[ "$current_major" != "$new_major" ]]; then
     bump_type="minor"
-else
+fi
 
 echo $bump_type
