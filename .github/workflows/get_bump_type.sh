967e5a8979f142ab0ef0126e4dc4d732a28f3a98
Dion Häfner <dion.haefner@simulation.science>
2025-12-02 13:17:18 +0100

    again

--- .github/workflows/get_bump_type.sh
+++ .github/workflows/get_bump_type.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-new_version=$(git-cliff --bumped-version)
+bump_type="auto"
 
 # Prevent bumping major versions, downgrade to minor in this case
+new_version=$(git-cliff --bumped-version --bump $bump_type)
 current_major=$(cut -d '.' -f 1 <<< "$current_version")
 new_major=$(cut -d '.' -f 1 <<< "$new_version")
 if [[ "$current_major" != "$new_major" ]]; then
-    bump_type=$(git-cliff --bumped-version --bump minor)
+    bump_type="minor"
 else
-    bump_type="auto"
-fi
 
 echo $bump_type
