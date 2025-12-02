0b60e31a91416aade5b83f73c265c79e5f0118bf
Dion Häfner <dion.haefner@simulation.science>
2025-12-02 13:00:13 +0100

    force bumped versions

--- .github/workflows/get_new_version.sh
+++ .github/workflows/get_new_version.sh
@@ -8,8 +8,7 @@ new_version=$(git-cliff --bumped-version)
 current_major=$(cut -d '.' -f 1 <<< "$current_version")
 new_major=$(cut -d '.' -f 1 <<< "$new_version")
 if [[ "$current_major" != "$new_major" ]]; then
-    echo "GIT_CLIFF__BUMP__BUMP_TYPE=minor" >> $GITHUB_ENV
-    new_version=$(git-cliff --bumped-version)
+    new_version=$(git-cliff --bumped-version --bump-type minor)
 fi
 
 echo $new_version
