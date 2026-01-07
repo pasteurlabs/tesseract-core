835078fe2bea21d326d7c9739d2ee7157961f3bb
Jonathan Brodrick <jonathanbrodrick@gmail.com>
2026-01-07 02:53:11 +0000

    use typing_extensions in dummy_tesseract to enable python 3.10 support

--- tests/dummy_tesseract/tesseract_api.py
+++ tests/dummy_tesseract/tesseract_api.py
@@ -1,10 +1,9 @@
 # Copyright 2025 Pasteur Labs. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Self
-
 import numpy as np
 from pydantic import BaseModel, Field, model_validator
+from typing_extensions import Self
 
 from tesseract_core.runtime import Array, Differentiable, Float32
 
