055b64928514adf735e58939021822b8540ef865
Alessandro Angioi <alessandro.angioi@simulation.science>
2026-03-13 13:03:27 +0100

    [drive-by change] Refactor get_non_base_fields_in_tesseract_config

--- tesseract_core/sdk/api_parse.py
+++ tesseract_core/sdk/api_parse.py
@@ -316,11 +316,10 @@ def get_config(src_dir: Path) -> TesseractConfig:
 
 
 def get_non_base_fields_in_tesseract_config() -> list[tuple[str, type]]:
-    """Gets fields in Tesseract Config that are not a base fields."""
-    base_fields = (str, int, float, bool, bytes, dict)
+    """Gets fields in TesseractConfig that are Pydantic sub-models."""
     non_base_fields = []
     for field_name, field_info in TesseractConfig.model_fields.items():
         origin = getattr(field_info.annotation, "__origin__", field_info.annotation)
-        if origin not in base_fields:
+        if isinstance(origin, type) and issubclass(origin, BaseModel):
             non_base_fields.append((field_name, field_info.annotation))
     return non_base_fields
