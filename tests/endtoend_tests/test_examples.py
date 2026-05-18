6ea7abb6aab3889112e168951db93dba69928941
Dion Häfner <dion.haefner@simulation.science>
2026-05-18 21:25:52 +0200

    add system_site_packages arg to tesseract config

--- tests/endtoend_tests/test_examples.py
+++ tests/endtoend_tests/test_examples.py
@@ -108,6 +108,7 @@ TEST_CASES = {
     ),
     "metrics": Config(test_with_random_inputs=True),
     "qp_solve": Config(),
+    "system_site_packages": Config(),
     "tesseractreference": Config(),  # Can't test requests standalone; needs target Tesseract. Covered in separate test.
     "userhandling": Config(),
 }
